b2ee9f06d1a0176173dc21597964262fa2e28eb2
Adriano Caetano <adrianu_f22@hotmail.com>
2020-09-07 22:02:15 -0300

    Refactor repository to use Tinydb to manipulate data

--- lib/model/service/MagnetRepository.py
+++ lib/model/service/MagnetRepository.py
@@ -1,52 +1,26 @@
 import json
-from urllib.parse import urlparse, parse_qs, urlunparse, urlencode
 from lib.model.Magnet import Magnet
 from typing import Union
+from tinydb import TinyDB, Query
 
-# @Todo Refactor to use TinyDB: https://pypi.org/project/tinydb/
 class MagnetRepository:
     storage = None
     storagePath = 'cache.json'
 
     def __init__(self):
-        with open(self.storagePath, 'r') as jsonFile:
-            self.storage = json.load(jsonFile)
+        self.storage = TinyDB(self.storagePath)
     
-    def save(self, entity: Magnet):
-        url = self._sanitize_url(entity.get_url())
-        
-        self.storage[url] = entity.to_dict()
+    def save(self, entity: Magnet):        
+        self.storage.insert(entity.to_dict())
 
-        with open(self.storagePath, 'w') as jsonFile:
-            json.dump(self.storage, jsonFile)
+    def findByUrl(self, url: str) -> Union[Magnet, None]:
+        MagnetQuery = Query()
+        document = self.storage.get(MagnetQuery.url == url)
 
-    def findByUrl(self, raw_url: str) -> Union[Magnet, None]:
-        url = self._sanitize_url(raw_url)
-        
-        if url in self.storage:
-            magnet = Magnet.from_dict(self.storage[url])
+        if document is None:
+            return None
 
-            if magnet.is_offline:
-                return None
-
-            return magnet            
-
-        return None
+        return Magnet.from_dict(document)
 
     def list(self) -> list:
-        magnets = []    
-        for _, magnet in self.storage.items():
-            magnets.append(magnet)
-
-        return magnets
-
-    def _sanitize_url(self, dirty_url) -> str:
-        parsedUrl = urlparse(dirty_url)
-        queryStrings = parse_qs(parsedUrl.query)
-        if 'ved' in queryStrings:
-            del queryStrings['ved']
-
-        urlParts = list(parsedUrl)
-        urlParts[4] = urlencode(queryStrings)
-
-        return urlunparse(urlParts)    
+        return self.storage.all()
